f9795e163c59ece39a4ce6f46424a51730559e13
jihoon <hun2ya9@gmail.com>
2021-10-03 21:28:46 +0900

    header 생성
    header 레이아웃 구성 완료
    header가 상단에 고정되도록 함
    hidden_bar를 header 하단에 추가

---
+++ img.js
@@ -0,0 +1,10 @@
+const imgData = [
+  "https://images.unsplash.com/photo-1604654894610-df63bc536371?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmFpbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1588359953494-0c215e3cedc6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmFpbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1571290274554-6a2eaa771e5f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmFpbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1590704634609-ba8981706635?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmFpbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1616427592814-195c30c24ea3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG5haWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1571728906312-8887e063d97f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG5haWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1612887390768-fb02affea7a6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5haWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1606247300685-1c2342a7a604?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5haWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+];
